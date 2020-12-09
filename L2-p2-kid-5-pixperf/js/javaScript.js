29d8ca55caa35915266afc9845431aa7199de591
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-12-09 01:34:33 +0100

    Push eerste werk
    Tempolate voor de opvolgende designs. Dit zal gaandeweg ook worden gedaan voor de javascript

---
+++ L2-p2-kid-5-pixperf/js/javaScript.js
@@ -0,0 +1,5 @@
+//Na het laden van de pagina, start de animatie van de Main blokje in het midden
+setTimeout( () => { 
+ document.querySelector('.ClassVanElement').classList.toggle('Pas_naam_aan_naar_slide_element');
+}, 300);
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# L2-p2-kid-5-pixperf
+Leerjaar 2, periode 2, Aangeleverde designs namaken
+
+# keuzedeel interaction design
+# live url:
+https://firesofdestruction.nl/bewijzenmap/kid/p2/vijf-designs/frame1/index.html
