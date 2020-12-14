d5bc03586948d64612d37e1ce41fc5ca322ec275
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2020-12-14 01:31:34 +0100

    Klikken op navigatie, veranderd dynamisch de class
    De volgende functies zijn toegevoegd op basis van eventListerner(click): 1:actief maken van de  navigatie waar op wordt geklikt, 2: verwijderen van de vorige actief class, wanneer een nieuwe class actief is

--- L2-p2-kid-5-pixperf/js/javaScript.js
+++ L2-p2-kid-5-pixperf/js/javaScript.js
@@ -1,5 +1,34 @@
-//Na het laden van de pagina, start de animatie van de Main blokje in het midden
-setTimeout( () => { 
- document.querySelector('.ClassVanElement').classList.toggle('Pas_naam_aan_naar_slide_element');
-}, 300);
+//Wanneer een slide al actief is, verwijder je het, zodat de volgende sectie geactiveerd kan worden
 
+let navigatieLinks = document.querySelectorAll('nav li a');
+
+const verwijderActiefNavigatieLink = () =>{
+ navigatieLinks.forEach( (link) =>{
+   //als er een classList is, verwijder die data
+  // link.classList.remove('actief');
+
+  if (link.classList = 'actief'){
+   link.classList.remove('actief');
+  }
+ });
+}
+
+//actief class toevoegen
+ const navigatieLinksActiefMaken = (elemen) => {
+  verwijderActiefNavigatieLink();
+  elemen.classList.add('actief');
+ }
+
+//Wanneer je op navigatie menu link klikt, zal de functie worden toegevoegd
+ navigatieLinks.forEach( (link) => {
+  link.addEventListener('click', (e) => {
+   //stop de actie, zodat het niet direct wordt uitgevoerd wanneer de DOM is geladen
+   e.preventDefault();
+   
+  //Vanaf dit punt wordt de functie opgeroepen om het element actief te maken
+  navigatieLinksActiefMaken(e.target);
+
+  //via de volgende regel link je door naar de juiste locatie, welke in de href staat: href='#id'
+  window.location = e.target.href;
+ })
+})
