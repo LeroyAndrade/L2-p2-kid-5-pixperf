a11bca9ccf5d67785f6bc2e9a3de1e73e0f85198
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2021-01-15 13:50:22 +0100

    upload half

--- L2-p2-kid-5-pixperf/js/javaScript.js
+++ L2-p2-kid-5-pixperf/js/javaScript.js
@@ -4,9 +4,8 @@
 
 //Ieder link in het menu
  //Ik kies hier bewust voor een volledige pad, zodat dit voorrang krijgt en niet wordt overwritten, door Bootstrap, wanneer ik daar ooit gebruik van maak.
-let navigatieLinks = document.querySelectorAll('nav li a');
-
-
+let navigatieLinksNav__Item = document.querySelectorAll('nav li a');
+//let navigatieLinksNav__Item = document.querySelectorAll('.nav__item');
 
 
 //De section in de HTML
@@ -17,28 +16,47 @@ let alleSecties = document.querySelectorAll('section');
 
 
 //
- let opties = {};
-
+ const opties = {};
 
 const doorsnee = (entries, observer) => {
- entries.forEach( entry =>{
-  console.log(entry.target + "doorsnede " + entry.isIntersecting);
+ entries.forEach( entry => {
+
+  //De classen moeten gewisseld worden in dit veld
+    //dit doe je op basis van de observe value
+    if (entry.isIntersecting){ //wanneer de intersecting gebeurd dan
+     let link = zoekBijpassendeLink(entry.target.id);
+     maakActief(link);
+    }
+  console.log(entry.target.id + "doorsnede " + entry.isIntersecting);
  })
 }
  //IntersectionObserver neemt twee argumenten --> https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver
  let observeren = new IntersectionObserver(doorsnee, opties); 
- observeren.observe(alleSecties[1]);
+
+ //Er zijn vijf menu items, en die plaats je in een for loop om ze allemaal, te targeten
+
+  //deze code zorgt er ook voor dat er een false of true komt als resultaat van de isIntersecting in de console
+ for (i =0; i < navigatieLinksNav__Item.length; i++){
+  observeren.observe(alleSecties[i]);
+ }
 
 //END IntersectionObserver // 
 
+maakActief = () =>{
+ alleSecties.forEach( (link) =>{
+  if (link.classList = 'actief'){
+   link.classList.remove('actief');
+  }
+ })
+}
 
 // verwijder Actief navigatie class //
 const verwijderActiefNavigatieLink = () =>{
- navigatieLinks.forEach( (link) =>{
+ navigatieLinksNav__Item.forEach( (link) =>{
    //als er een classList is, verwijder die data
   // link.classList.remove('actief');
 
-  if (link.classList = 'actief'){
+  if (link.classList.contains('actief')){
    link.classList.remove('actief');
   }
  });
@@ -51,7 +69,7 @@ const verwijderActiefNavigatieLink = () =>{
  }
 
 //Wanneer je op navigatie menu link klikt, zal de functie worden toegevoegd
- navigatieLinks.forEach( (link) => {
+navigatieLinksNav__Item.forEach( (link) => {
   link.addEventListener('click', (e) => {
    //stop de actie, zodat het niet direct wordt uitgevoerd wanneer de DOM is geladen
    e.preventDefault();
@@ -63,3 +81,15 @@ const verwijderActiefNavigatieLink = () =>{
   window.location = e.target.href;
  })
 })
+
+//geef element terug zodat het de id krijgt van de observer
+const zoekBijpassendeLink = (id) => {
+ let link = document.querySelector('nav li a[href="' + id + '"]');
+ console.log(link + '-')
+
+ return link;
+
+}
+
+
+//bekijk de callback functie die de andere functie oproept  -> zoekBijpassendeLink moet in iedergeval worden opgeroepen
